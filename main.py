575cb550f1cee51a48c58cc78dcb3aeadd392f5c
ortariot <ortariot@gmail.com>
2021-05-11 19:02:40 +0700

    second case completed

--- main.py
+++ main.py
@@ -1,22 +1,48 @@
-with open('recipes.txt', encoding='utf-8') as f:
-    out = {}
-    for line in f:
-        dish = line.strip()
-        num = int(f.readline().strip())
-        i = 0
-        ingr = []
-        while True:
-            if i == num:
-                break
-            i += 1
-            ingr_str = f.readline().strip()
-            row = {}
-            row['ingredient_name'] = ingr_str[0 : ingr_str.find(' | ')]
-            row['quantity'] = int(ingr_str[ingr_str.find(' | ') + 3 : ingr_str.rfind(' | ')])
-            row['measure'] = ingr_str[ingr_str.rfind(' | ') + 3:]
-            ingr.append(row)
-        out[dish] = ingr
-        f.readline()
+from pprint import pprint
+
+class CookHelper:
+    def __init__(self):
+        with open('recipes.txt', encoding='utf-8') as f:
+            self.cook_book = {}
+            for line in f:
+                dish = line.strip()
+                num = int(f.readline())
+                i = 0
+                ingr = []
+                while i < num:
+                    i += 1
+                    ingr_str = f.readline().strip()
+                    row = {
+                        'ingredient_name' : ingr_str[0 : ingr_str.find(' | ')],
+                        'quantity' : int(ingr_str[ingr_str.find(' | ') + 3 : ingr_str.rfind(' | ')]),
+                        'measure' : ingr_str[ingr_str.rfind(' | ') + 3:]
+                        }
+                    ingr.append(row)
+                self.cook_book[dish] = ingr
+                f.readline()
+    
+    def cook_book_view(self):
+        pprint(self.cook_book) 
+    
+    def get_shop_list_by_dishes(self, dishes, person_count):
+        shop_list = {}
+        for dish in dishes:
+            if dish in self.cook_book:
+                for row in self.cook_book[dish]:
+                    if shop_list.get('ingredient_name') is None:
+                        shop_list[row['ingredient_name']] = {'measure' : row['measure'], 
+                                                            'quantity' : row['quantity'] * person_count}
+                    else: 
+                        shop_list['ingredient_name']['quantity'] += row['quantity'] * person_count   
+            else: 
+                print(f"Рецепт {dish} в вашей книге рецептов не обнаружен")
+        return shop_list
 
 
-    print(out)
+if __name__ == '__main__':
+    cook_session = CookHelper()
+    
+    sl = cook_session.get_shop_list_by_dishes(['Омлет', 'Фахитос', 'Омлет'], 2)
+    pprint(sl)
+
+
